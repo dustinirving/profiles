93f8ca6a5a65a88a84b38ea49a00e3ed40000ac9
Dustin Irving <dustin.irving@gmail.com>
2020-05-01 21:14:45 -0400

    Fix deployment

--- server.js
+++ server.js
@@ -34,8 +34,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client/build")));
-
 app.get("/api/people", async function (req, res) {
   const [rows] = await db.query(`SELECT * FROM user_info`);
   return res.json(rows);
@@ -49,9 +47,12 @@ app.post("/api/people", async function (req, res) {
   });
   return res.json(user);
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`App is listening on PORT ${PORT}`));
